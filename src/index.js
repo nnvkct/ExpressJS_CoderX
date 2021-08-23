5b14a96df581ec680764effeeef5be8ce3f733e7
unknown <nnvkct@yahoo.com>
2021-08-23 14:21:10 +0700

    add chuc nang search

--- src/index.js
+++ src/index.js
@@ -6,12 +6,29 @@ const port = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+var users = [
+  { id: 1, name: "Thinh" },
+  { id: 2, name: "Toan" },
+  { id: 3, name: "Hung" }
+];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { name: "NNV" });
 });
 
 app.get("/users", function (req, res) {
-  res.send("User list not available!");
+  res.render(path.join(__dirname, "views/users/index"), { users });
+});
+
+app.get("/users/search", function (req, res) {
+  console.log("NNV is testing");
+  var { query } = req;
+  var matchUser = users.filter(function (user) {
+    return user.name.toLowerCase().indexOf(query.q.toLowerCase()) !== -1;
+  });
+
+  res.render(path.join(__dirname, "views/users/index"), { users: matchUser });
+  console.log(matchUser);
 });
 
 app.listen(port, () => {
