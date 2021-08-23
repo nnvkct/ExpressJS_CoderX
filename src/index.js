1eb5dcbe056f2103b5b55c2dd829df369202b06e
unknown <nnvkct@yahoo.com>
2021-08-23 14:24:20 +0700

    add chuc nang tao user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
@@ -12,6 +14,9 @@ var users = [
   { id: 3, name: "Hung" }
 ];
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get("/", (req, res) => {
   res.render("index", { name: "NNV" });
 });
@@ -21,14 +26,21 @@ app.get("/users", function (req, res) {
 });
 
 app.get("/users/search", function (req, res) {
-  console.log("NNV is testing");
   var { query } = req;
   var matchUser = users.filter(function (user) {
     return user.name.toLowerCase().indexOf(query.q.toLowerCase()) !== -1;
   });
 
   res.render(path.join(__dirname, "views/users/index"), { users: matchUser });
-  console.log(matchUser);
+});
+
+app.get("/users/create", function (req, res) {
+  res.render(path.join(__dirname, "views/users/create"));
+});
+
+app.post("/users/create", function (req, res) {
+  users.push(req.body);
+  res.redirect("/users");
 });
 
 app.listen(port, () => {
